1d826a30ab2f1a6ba65f5158cc0f769bb383ba1a
Dmitry Smirnov <dsmi@outlook.com>
2025-05-12 09:08:50 -0600

    Modify scripts which plots basis fuction to experiment with the surface integrals

--- plot_vector_bf.m
+++ plot_vector_bf.m
@@ -1,8 +1,8 @@
 
-r = [ 0, 1, 0 ; ...
-      0, 0, 0 ; ...
-      1, 0.5, 0 ; ...
-      0.5, 0.5, 1 ];
+r = [ 0.1, 1, 0.2 ; ...
+      -0.1, 0.3, -0.2 ; ...
+      1, 0.5, -0.1 ; ...
+      0.5, 0.4, 1 ];
 
 tetra = [ 1 2 3 4 ];
 
@@ -14,13 +14,33 @@ rt = r( tetra(1,:), : );
         
 [f, g] = tetra_fg(rt);
 
-% test points
-p=simplexquad(3,rt);
+%% % test points
+%% p=simplexquad(3,rt);
+
+qN = 3;
+[ qA qW ] = simplexquad(qN, 2);
+qA = [ qA 1-sum(qA,2) ]; % barycentric coordinates
+
+v1 = rt(1,:);
+v2 = rt(2,:);
+v3 = rt(4,:);
+
+p = v1.*qA(:,1) + v2.*qA(:,2) + v3.*qA(:,3);
 
 np = size(p,1);
 
 % vector basis function calculated via fg
 Nj = repmat(f(j,:),np,1) + cross(repmat(g(j,:),np,1),p,2);
+
+% Normal and area
+n = cross( (v1-v3), (v2-v3), 2 ); % non-normalized
+A = sqrt( sum ( n.*n, 2 ) ) / 2 % triangle area
+n = repmat( n ./ (2*A), size(Nj, 1), 1 ); % normalized
+
+% Length of the edge
+l = sqrt( sum( (v1-v2).*(v1-v2), 2 ) )
+
+nxnj = cross( n, Nj, 2 );
 
 
 [ i1, i2 ] = tetra_edge_verts( j );
@@ -36,9 +56,16 @@ faces=[tetra(:,[1,2,3]);
 
 scatter3( rt([ i1, i2 ],1), rt([ i1, i2 ],2), rt([ i1, i2 ],3), 'b')
 hold on
-%% scatter3( p(:,1), p(:,2), p(:,3), 'r')
+%% scatter3( p(:,1), p(:,2), p(:,3), 'c')
+scatter3( [ v1(:,1) v2(:,1) v3(:,1)], ...
+          [ v1(:,2) v2(:,2) v3(:,2)], ...
+          [ v1(:,3) v2(:,3) v3(:,3)],'c')
 
 quiver3( p(:,1), p(:,2), p(:,3), Nj(:,1), Nj(:,2), Nj(:,3), 'r' );
+
+quiver3( p(:,1), p(:,2), p(:,3), n(:,1), n(:,2), n(:,3), 'm' );
+
+quiver3( p(:,1), p(:,2), p(:,3), nxnj(:,1), nxnj(:,2), nxnj(:,3), 'b' );
 
 % plot edges
 for j=1:6
