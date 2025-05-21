32e868b6e7d7bdd74851bda8bc606a914b4a73e8
Dmitry Smirnov <dsmi@outlook.com>
2025-05-20 22:44:52 -0600

    Modify plot_vector_bf to experiment with surface integrals

--- plot_vector_bf.m
+++ plot_vector_bf.m
@@ -42,6 +42,9 @@ l = sqrt( sum( (v1-v2).*(v1-v2), 2 ) )
 
 nxnj = cross( n, Nj, 2 );
 
+% Another way to compute it
+nxnj2 = (v3-p)*l/(2*A);
+
 
 [ i1, i2 ] = tetra_edge_verts( j );
 
@@ -54,12 +57,20 @@ faces=[tetra(:,[1,2,3]);
 
 %% trimesh( faces, r(:,1), r(:,2), r(:,3) )
 
-scatter3( rt([ i1, i2 ],1), rt([ i1, i2 ],2), rt([ i1, i2 ],3), 'b')
-hold on
-%% scatter3( p(:,1), p(:,2), p(:,3), 'c')
+% vertices of the face we are processing
 scatter3( [ v1(:,1) v2(:,1) v3(:,1)], ...
           [ v1(:,2) v2(:,2) v3(:,2)], ...
-          [ v1(:,3) v2(:,3) v3(:,3)],'c')
+          [ v1(:,3) v2(:,3) v3(:,3)],'r')
+hold on
+
+% vertices of the basis edge
+scatter3( rt([ i1, i2 ],1), rt([ i1, i2 ],2), rt([ i1, i2 ],3), 'b')
+
+%% scatter3( [ v2(:,1) ], ...
+%%           [ v2(:,2) ], ...
+%%           [ v2(:,3) ],'b')
+
+%% scatter3( p(:,1), p(:,2), p(:,3), 'c')
 
 quiver3( p(:,1), p(:,2), p(:,3), Nj(:,1), Nj(:,2), Nj(:,3), 'r' );
 
@@ -68,9 +79,9 @@ quiver3( p(:,1), p(:,2), p(:,3), n(:,1), n(:,2), n(:,3), 'm' );
 quiver3( p(:,1), p(:,2), p(:,3), nxnj(:,1), nxnj(:,2), nxnj(:,3), 'b' );
 
 % plot edges
-for j=1:6
-    [ i1, i2 ] = tetra_edge_verts( j );
-    plot3( rt([ i1, i2 ],1), rt([ i1, i2 ],2), rt([ i1, i2 ],3), '-g')
+for pj=1:6
+    [ k1, k2 ] = tetra_edge_verts( pj );
+    plot3( rt([ k1, k2 ],1), rt([ k1, k2 ],2), rt([ k1, k2 ],3), '-g')
 end
 
 xlabel('x')
